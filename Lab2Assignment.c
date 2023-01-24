5cb86489191bf812148291c436495551b354aac6
evanjohns <evanlloydjohns@gmail.com>
2023-01-24 15:41:57 -0500

    Got everything working

--- Lab2Assignment.c
+++ Lab2Assignment.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -7,73 +8,75 @@
 #include <errno.h>
 
 int main(int argc, char* argv[]) {
-    int running = 1;
 
-    while (running) {
-        pid_t pid, child;
-        int status;
+    while (1) {
+        pid_t pid;
+        struct rusage usage_before, usage_after;
+        char input[256];
+        char* argument_list[256];
 
-        char in1[100];
-        printf("Please enter a command: ");
-        fgets(in1, 100, stdin);
-        printf("input: %s\n", in1);
-        char in2[100];
-        strcpy(in2, in1);
+        // Get user input
+        printf("Enter a string: ");
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
 
-        char* tk1;
-        int c = 0;
-        tk1 = strtok(in1, " ");
-        while (tk1 != NULL) {
-            c++;
-//            printf("%s\n", tk1);
-            tk1 = strtok(NULL, " ");
+        // Parse user input into array
+        char* token = strtok(input, " ");
+        int argumentListLength = 0;
+        while (token != NULL) {
+            argument_list[argumentListLength++] = token;
+            token = strtok(NULL, " ");
         }
 
-        char* argument_list[c];
-        char* tk2;
-        tk2 = strtok(in2, " ");
-        for (int i = 0; tk2 != NULL; i++) {
-            strcpy(argument_list[i], tk2);
-//            printf("%s\n", tk2);
-            tk2 = strtok(NULL, " ");
+        // Make array NULL terminated
+        argument_list[argumentListLength] = NULL;
+
+        // Check for exit condition
+        if (strcmp(argument_list[0], "quit") == 0) {
+            break;
         }
 
-        printf("size: %d\n", c);
-        for (int j = 0; j < c; j++) {
-            printf("%s\n", argument_list[j]);
+        // Get usage statistics before
+        getrusage(RUSAGE_CHILDREN, &usage_before);
+
+        // Fork and make sure it worked
+        if ((pid = fork()) < 0) {
+            fprintf(stderr, "Fork failure: %s", strerror(errno));
+            exit(1);
         }
 
-//        if ((pid = fork()) < 0) {
-//            fprintf(stderr, "Fork failure: %s", strerror(errno));
-//            exit(1);
-//        } else if (pid == 0) {
-//            if (execvp(argument_list[0], &argument_list[0]) < 0) {
-//                perror("exec failed");
-//                exit(1);
-//            }
-//        } else {
-//            child = wait(&status);
-//            status = WEXITSTATUS(status);
-//            printf("Child PID: %ld terminated with return status %d\n", (long) child, status);
-//        }
+        // Handle for the child
+        if (pid == 0) {
+            // Execute command and check for errors
+            int status = execvp(argument_list[0], &argument_list[0]);
+            if (status < 0) {
+                perror("exec failed");
+                exit(1);
+            }
+        // Handle for the parent
+        } else {
+            int status;
+            waitpid(pid, &status, 0);
+
+            // Ensure child exited properly
+            if (WIFEXITED(status)) {
+
+                // Get usage statistics after child process is terminated
+                getrusage(RUSAGE_CHILDREN, &usage_after);
+
+                // Calculate the difference between the cpu time values
+                struct timeval child_time = {usage_after.ru_utime.tv_sec - usage_before.ru_utime.tv_sec,
+                                             usage_after.ru_utime.tv_usec - usage_before.ru_utime.tv_usec};
+
+                // Calculate the difference between the involuntary context switches
+                long int nivcsw = usage_after.ru_nivcsw - usage_before.ru_nivcsw;
+
+                // Display statistics
+                printf("Child Process %d\n", pid);
+                printf("\tCPU time used: %dms\n", child_time.tv_usec);
+                printf("\tInvoluntary Context Switches: %ld\n", nivcsw);
+            }
+        }
     }
+    return 0;
 }
-
-/*
-    while running
-        get user input
-        if input == exit
-                running = 0
-        if ((pid = fork) < 0)
-            There was an issue
-        if (pid == 1)
-            We are a child
-            use execvp() to call terminal
-            If execvp didn't work
-                print stuff and exit with bad code
-        else
-            wait()
-            use some resource statistics stuff to get info from child's process
-            print that out
-    return 0
-*/
--- test.c
+++ test.c
@@ -6,47 +6,71 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-int main(int argc, char* argv[]) {
-    char input[100];
-    printf("Please enter stuff: ");
-    fgets(input, 100, stdin);
-    printf("input: %s\n", input);
-    char in2[100];
-    strcpy(in2, input);
+#include <stdio.h>
+#include <string.h>
 
-    // https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
-    char* tk1;
-    int c = 0;
-    tk1 = strtok(input, " ");
-    while (tk1 != NULL) {
-        c++;
-        printf("%s\n", tk1);
-        tk1 = strtok(NULL, " ");
+int main() {
+    char input[256];
+    printf("Enter a string: ");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0; // remove newline character
+
+    char *argument_list[256];
+    char *token = strtok(input, " ");
+    int argumentListLength = 0;
+    while (token != NULL) {
+        argument_list[argumentListLength++] = token;
+        token = strtok(NULL, " ");
     }
 
-    printf("c: %d\n", c);
-    printf("input: %s\n", input);
-
-    c = 0;
-    tk1 = strtok(in2, " ");
-    while (tk1 != NULL) {
-        c++;
-        printf("%s\n", tk1);
-        tk1 = strtok(NULL, " ");
-    }
-    printf("c: %d\n", c);
-    printf("input: %s\n", in2);
-
-//    char* argument_list[c];
-//    char* tk2;
-//    tk2 = strtok(input, " ");
-//    for (int i = 0; tk2 != NULL; i++) {
-//        strcpy(argument_list[i], tk2);
-//        printf("%s\n", tk2);
-//        tk2 = strtok(NULL, " ");
+//    for (int i = 0; i < argumentListLength; i++) {
+//        printf("Word %d: %s\n", i+1, argument_list[i]);
 //    }
-//    for (int i = 0; i < c; i++) {
-//        printf("%s", argument_list[i]);
-//    }
-    printf("done\n");
+
+    return 0;
 }
+
+//int main(int argc, char* argv[]) {
+//    char input[100];
+//    printf("Please enter stuff: ");
+//    fgets(input, 100, stdin);
+//    printf("input: %s\n", input);
+//    char in2[100];
+//    strcpy(in2, input);
+//
+//    // https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
+//    char* tk1;
+//    int c = 0;
+//    tk1 = strtok(input, " ");
+//    while (tk1 != NULL) {
+//        c++;
+//        printf("%s\n", tk1);
+//        tk1 = strtok(NULL, " ");
+//    }
+//
+//    printf("c: %d\n", c);
+//    printf("input: %s\n", input);
+//
+//    c = 0;
+//    tk1 = strtok(in2, " ");
+//    while (tk1 != NULL) {
+//        c++;
+//        printf("%s\n", tk1);
+//        tk1 = strtok(NULL, " ");
+//    }
+//    printf("c: %d\n", c);
+//    printf("input: %s\n", in2);
+//
+////    char* argument_list[c];
+////    char* tk2;
+////    tk2 = strtok(input, " ");
+////    for (int i = 0; tk2 != NULL; i++) {
+////        strcpy(argument_list[i], tk2);
+////        printf("%s\n", tk2);
+////        tk2 = strtok(NULL, " ");
+////    }
+////    for (int i = 0; i < c; i++) {
+////        printf("%s", argument_list[i]);
+////    }
+//    printf("done\n");
+//}
